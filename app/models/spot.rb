626388ef4ac145517fd00c993e62a553484f571c
Anthony Johnson <anthonythomasjohnson@outlook.com>
2021-08-23 16:47:47 +0900

    commented out geocode

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -4,8 +4,8 @@ class Spot < ApplicationRecord
   has_many :videos
   has_one_attached :photo
 
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
+  # geocoded_by :address
+  # after_validation :geocode, if: :will_save_change_to_address?
 
   validates :name, presence: true
   validates :address, presence: true

143bfa3a2fca90390f881bc06169cb07f43849cf
meljun picardal <meljun.one@gmail.com>
2021-08-24 11:25:11 +0900

    Modified Spot model to has_many_attached photos

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -2,7 +2,7 @@ class Spot < ApplicationRecord
   belongs_to :user
   has_many :reviews
   has_many :videos
-  has_many_attached :photo
+  has_many_attached :photos
 
   # geocoded_by :address
   # after_validation :geocode, if: :will_save_change_to_address?

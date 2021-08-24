f9a95cdf49ffac213663b7147ac9686aae8b938d
Anthony Johnson <anthonythomasjohnson@outlook.com>
2021-08-24 11:25:18 +0900

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :spots, only: [:index, :show, :new, :create] do
+    resources :videos, only: [:new, :create]
+    resources :reviews, only: [:create]
+  end
+
+  resources :users, only: [:show]
+
+  resources :videos, only: [:update] do
+    resources :reviews, only: [:create]
+  end
 end

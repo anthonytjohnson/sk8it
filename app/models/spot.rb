726588a0f1a0086ac47534bd816037980ec6c1bb
Derek Torres <derek.n.torres@gmail.com>
2021-08-24 16:02:21 +0900

    users can upload a spot

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -7,10 +7,12 @@ class Spot < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
+  CATEGORIES = ["spot", "shop", "park"]
   validates :name, presence: true
   validates :address, presence: true
   validates :description, presence: true
   validates :category, presence: true
+  validates :category, inclusion: { in: CATEGORIES }
   validates :name, uniqueness: true
   validates :description, length: { within: 10..140 }
 end
--- app/policies/spot_policy.rb
+++ app/policies/spot_policy.rb
@@ -5,6 +5,10 @@ class SpotPolicy < ApplicationPolicy
     end
   end
 
+  def create?
+    true
+  end
+
   def show?
     true
   end

db92094306472b99d0600035a20cc36f966e2b69
Anthony Johnson <anthonythomasjohnson@outlook.com>
2021-08-29 11:16:29 +0900

    Review create added in controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,20 @@
 class ReviewsController < ApplicationController
+  def create
+    @spot = Spot.find(params[:spot_id])
+    @review = Review.new(review_params)
+    @review.user = current_user
+    @review.spot = @spot
+    authorize @review
+    if @review.save
+      redirect_to @spot, notice: 'Review added!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:comment, :rating)
+  end
 end

a04da0b471f0739ea9632729526491760258543d
Anthony Johnson <anthonythomasjohnson@outlook.com>
2021-08-30 16:54:03 +0900

    added create method in comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,22 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    @video = Video.find(params[:video_id])
+    @comment.user = current_user
+    @spot = video.spot
+    @comment.video = @video
+    authorize @comment
+
+    if @comment.save
+      redirect_to spot_path(@spot), notice: 'Comment added!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment)
+  end
 end

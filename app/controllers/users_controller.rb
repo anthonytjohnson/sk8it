baa1f9c7193ebf3037a029523da43cbd62e89a3a
Anthony Johnson <anthonythomasjohnson@outlook.com>
2021-08-30 12:18:32 +0900

    commit 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
-  before_action :authorize_user!
+
+
+  def show
+    @user = User.find(params[:id])
+  end
 
   def authorize_user
     authorize @user = current_user

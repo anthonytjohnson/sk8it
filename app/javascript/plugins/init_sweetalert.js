3422b392a77f2cd4e1e5d1cb9a711e647e0b197f
Derek Torres <derek.n.torres@gmail.com>
2021-09-02 14:32:31 +0900

    shops don't show check in alert

--- app/javascript/plugins/init_sweetalert.js
+++ app/javascript/plugins/init_sweetalert.js
@@ -3,20 +3,22 @@ import Rails from '@rails/ujs';
 
 
 const initSweetAlert = () => {
-  if (document.querySelector("#check-in-form")) {
-    Swal.fire({
-      title: "Do you want to check in to this spot?",
-      confirmButtonText: `Yes`,
-      showDenyButton: true,
-      denyButtonText: `No`,
-    }).then((result) => {
-      if (result.isConfirmed) {
-        const form = document.querySelector("#check-in-form");
-        Rails.fire(form, 'submit');
-      } else {
-        console.log("nada");
-      }
-    })
+  if (document.getElementById("spot-div")) {
+    if (document.querySelector("#check-in-form")) {
+      Swal.fire({
+        title: "Do you want to check in to this spot?",
+        confirmButtonText: `Yes`,
+        showDenyButton: true,
+        denyButtonText: `No`,
+      }).then((result) => {
+        if (result.isConfirmed) {
+          const form = document.querySelector("#check-in-form");
+          Rails.fire(form, 'submit');
+        } else {
+          console.log("nada");
+        }
+      })
+    }
   }
 }
 

dc3dc7c20be38189bddd67786f243f31e50f4183
Anthony Johnson <anthonythomasjohnson@outlook.com>
2021-09-02 15:53:41 +0900

    added js to toggle faovrites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,11 +2,13 @@ class FavoritesController < ApplicationController
   def create
     @spot = Spot.find(params[:spot_id])
     @favorite = Favorite.create(user_id: current_user.id, spot_id: @spot.id)
+    authorize @favorite
   end
 
   def destroy
     @spot = Spot.find(params[:spot_id])
-    @favorite = Favorite.find_by(user_id: current_user.id, post_id: @post.id)
+    @favorite = Favorite.find_by(user_id: current_user.id, spot_id: @spot.id)
+    authorize @favorite
     @favorite.destroy
   end
 end
